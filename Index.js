aa8f2fc62384f0a274ccf65254e0bb506e3684e1
mario sousa <vegetalizado@gmail.com>
2021-04-08 16:50:49 +0100

    final push

--- Index.js
+++ Index.js
@@ -10,11 +10,11 @@ let fastPoopies = [];
 let gameOver = false;
 let intervalId;
 let powerUpPoints = 0;
-
 const gameArea = {
   frames: 0,
   points: 0,
-
+  showMinusPoints: false,
+  showExtraPoints: false,
   stop: function () {
     cancelAnimationFrame(intervalId);
   },
@@ -30,7 +30,14 @@ const gameArea = {
     context.fillText(`${this.points} pts.`, 550, 175);
   },
   restart: function () {
+   // location.reload()
     context.clearRect(0, 0, canvas.width, canvas.height);
+    
+
+
+rainbowPoopies = [];
+
+    powerUpPoints = 0;
     gameOver = false;
     player.lives = 3;
     player.x = 0;
@@ -38,9 +45,11 @@ const gameArea = {
     stars = [];
     fastPoopies = [];
     this.frames = 0;
-    this.score();
+    this.points = 0;
+    this.score()
     document.getElementById("restartGame").style.display = "none";
     updateGameArea();
+    
   },
   score: function () {
     //calcular os pontos pela soma de frames, que não pára a não ser que seja game over
@@ -50,11 +59,23 @@ const gameArea = {
     context.fillStyle = "white";
     context.fillText(`score: ${this.points}`, 50, 50);
   },
+  minusPoints: function () {
+    if (gameArea.showMinusPoints) {
+      context.font = "20px Impact";
+      context.fillStyle = "black";
+      context.fillText(`-100 pts.`, 50, 75);
+    }
+  },
+  extraPoints: function () {
+    if (gameArea.showExtraPoints) {
+      context.font = "20px Impact";
+      context.fillStyle = "black";
+      context.fillText(`+100 pts.`, 50, 75);
+    }
+  }
 };
-
 const backgroundImg = new Image();
 backgroundImg.src = "./public/images/finalBackG3.jpg";
-
 const backgroundImage = {
   image: backgroundImg,
   x: 0,
@@ -68,7 +89,6 @@ const backgroundImage = {
     context.drawImage(this.image, this.x + canvas.width, 0); //Q. Hein?
   },
 };
-
 function hitBottom() {
   //declara que o jogador chega ao fundo e não o ultrapassa
   let rockBottom = canvas.height - player.height - 41;
@@ -77,7 +97,6 @@ function hitBottom() {
     player.vy = 0;
   }
 }
-
 function updateGameArea() {
   backgroundImage.move();
   gameArea.clear();
@@ -86,18 +105,24 @@ function updateGameArea() {
   player.gravity();
   player.drawUnicorn();
   player.drawLives();
+  gameArea.minusPoints();
+  gameArea.extraPoints();
   updateObstacles();
   checkGameOver();
   checkStar();
   checkRainbowPoopy();
+  if (gameArea.frames % 200 === 0) {
+    gameArea.showMinusPoints = false;
+  }
+  if (gameArea.frames % 200 === 0) {
+    gameArea.showExtraPoints = false;
+  }
   if (!gameOver) {
     gameArea.score();
     intervalId = requestAnimationFrame(updateGameArea);
   }
 }
-
 const player = new Player();
-
 document.addEventListener("keydown", (e) => {
   e.preventDefault();
   if (gameOver === false) {
@@ -118,7 +143,6 @@ document.addEventListener("keyup", (e) => {
     player.userPull = 0;
   }
 });
-
 function updateObstacles() {
   for (let i = 0; i < obstacles.length; i++) {
     obstacles[i].x -= 2;
@@ -126,13 +150,49 @@ function updateObstacles() {
   }
   for (let i = 0; i < fastPoopies.length; i++) {
     fastPoopies[i].x -= 4;
+    if (
+      (fastPoopies[i].y > 160 &&
+        fastPoopies[i].x < 1000 &&
+        fastPoopies[i].x > 900) ||
+      (fastPoopies[i].y > 160 &&
+        fastPoopies[i].x < 800 &&
+        fastPoopies[i].x > 700) ||
+      (fastPoopies[i].y > 160 &&
+        fastPoopies[i].x < 600 &&
+        fastPoopies[i].x > 500) ||
+      (fastPoopies[i].y > 160 &&
+        fastPoopies[i].x < 400 &&
+        fastPoopies[i].x > 300) ||
+      (fastPoopies[i].y > 160 &&
+        fastPoopies[i].x < 200 &&
+        fastPoopies[i].x > 100)
+    ) {
+      fastPoopies[i].y -= 4;
+    } else if (
+      (fastPoopies[i].y < 370 &&
+        fastPoopies[i].x > 0 &&
+        fastPoopies[i].x < 100) ||
+      (fastPoopies[i].y < 370 &&
+        fastPoopies[i].x > 200 &&
+        fastPoopies[i].x < 300) ||
+      (fastPoopies[i].y < 370 &&
+        fastPoopies[i].x > 400 &&
+        fastPoopies[i].x < 500) ||
+      (fastPoopies[i].y < 370 &&
+        fastPoopies[i].x > 600 &&
+        fastPoopies[i].x < 700) ||
+      (fastPoopies[i].y < 370 &&
+        fastPoopies[i].x > 800 &&
+        fastPoopies[i].x < 900)
+    ) {
+      fastPoopies[i].y += 4;
+    }
     fastPoopies[i].update();
   }
   for (let i = 0; i < stars.length; i++) {
     stars[i].x -= 4;
     stars[i].update();
   }
-
   gameArea.frames += 1;
   let positionArray = [350, 500, 700, 800]; //espaçamento
   const gap = Math.floor(Math.random() * positionArray.length);
@@ -146,8 +206,7 @@ function updateObstacles() {
     );
     obstacles.push(obstacleBottom);
   }
-
-  if (gameArea.frames % 100 === 0) {
+  if (gameArea.frames % 1200 === 0) {
     let height = 150;
     let otherObstacleBottom = new fastPoop(
       150,
@@ -157,7 +216,6 @@ function updateObstacles() {
     );
     fastPoopies.push(otherObstacleBottom);
   }
-
   if (gameArea.frames % 1000 === 0) {
     let height = 70;
     let randomY = Math.floor(Math.random() * (250 - 80 + 1) + 80);
@@ -165,19 +223,22 @@ function updateObstacles() {
     stars.push(obstacleTop);
   }
 }
-
 function checkRainbowPoopy() {
   let rainbowPoopyIndex;
-  const rainbowCrash = rainbowPoopies.some((rainbowPoopy, index) => {
+  const rainbowCrash = fastPoopies.some((rainbowPoopy, index) => {
     rainbowPoopyIndex = index;
     return rainbowPoopy.crashWith(player);
   });
-  if (rainbowCrash) {
-    powerUpPoints -= 100;
-    rainbowPoopies.splice(rainbowPoopyIndex, 1);
-  }
-}
 
+  if (rainbowCrash) {
+    if (gameArea.points > 100) {
+      powerUpPoints -= 100;
+      fastPoopies.splice(rainbowPoopyIndex, 1);
+      gameArea.showMinusPoints = true;
+    }
+  }
+
+}
 function checkStar() {
   let starIndex;
   const starCrash = stars.some((star, index) => {
@@ -187,10 +248,10 @@ function checkStar() {
   if (starCrash) {
     powerUpPoints += 100;
     stars.splice(starIndex, 1);
+    gameArea.showExtraPoints = true;
     player.handleCelebration();
   }
 }
-
 function checkGameOver() {
   const crashed = obstacles.some((obstacle, index) => {
     if (obstacle.crashWith(player) && player.lives < 2) {
@@ -210,19 +271,16 @@ function checkGameOver() {
     document.getElementById("restartGame").style.display = "block";
   }
 }
-
 document.getElementById("startGame").onclick = () => {
   document.getElementById("gameIntro").style.display = "none";
   document.getElementById("game").style.display = "grid";
-
+  backgroundMusic.loop = true;
   backgroundMusic.play();
   updateGameArea();
 };
-
 function restartGame() {
   gameArea.restart();
 }
-
 /* function gainPoints() {
   document.getElementById("gameIntro").style.display = "none";
 } */
